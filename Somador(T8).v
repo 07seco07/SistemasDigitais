cc7a679085ca2a608b7f8bedba78bea9626a6168
Felipe da Silva <felipe.ifsc@gmail.com>
2017-07-05 09:19:54 -0300

    Update Somador(T8).v

--- Somador(T8).v
+++ Somador(T8).v
@@ -1,35 +1,46 @@
 module somador(input clock,
-	input [43:0] E1,
-	input [43:0] E2,
-	output[43:0] v_v
-	);
+	       input [43:0] a,
+	       input [43:0] b,
+	       output[43:0] s
+		);
 reg [43:0] saida;
-assign v_v = saida;
+assign s = saida;
 always @(posedge clock) begin
-	saida <= E1 + E2;
+	saida <= a + b;
 end
 endmodule
+
 module teste;
+	
 	reg clock;
-	reg[43:0] E1, E2;
-	wire[43:0] v_v;
+	reg[43:0] a, b;
+	
+	wire[43:0] s;
+	
 	always #2 clock = ~clock;
-	somador B(clock, E1, E2, v_v);
+	
+	somador B(clock, a, b, s);
+	
 	initial begin
-		$dumpvars(0, B);
-#0 clock <= 0;
-E1 <= 4;
-E2 <= 8;
-#30
-E1 <= 11;
-E2 <= 40;
-#60
-E1 <= 4;
-E2 <= 5;
-#90;
-E1 <= 38;
-E2 <= 62;
-#500;
-$finish;
+	$dumpvars(0, B);
+		
+	#0 clock <= 0;
+	a <= 4;
+	b <= 8;
+		
+	#30
+	a <= 11;
+	b <= 40;
+		
+	#60
+	a <= 4;
+	b <= 5;
+		
+	#90;
+	a <= 38;
+	b <= 62;
+		
+	#500;
+	$finish;
 end
 endmodule
